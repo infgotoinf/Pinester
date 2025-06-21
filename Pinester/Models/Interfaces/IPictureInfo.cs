f1a7f855102e29d295e1d86f67909361a3399e03
DemDanEm <scholschit@gmail.com>
2025-06-21 21:32:33 +0300

    bit of mvvvm, PictureMainColor, nothing tested, cause non of you did anything

---
+++ Pinester/Models/Interfaces/IPictureInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinester.Models.Interfaces
+{
+    internal interface IPictureInfo
+    {
+        static abstract Dictionary<string, int> GetInfo(Bitmap picture);
+    }
+}
